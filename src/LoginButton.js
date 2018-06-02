c918453f3756287609514229909db4bcf9f1b269
Rodrigo Cavichioli <rodrigoack@gmail.com>
2018-06-02 19:12:59 -0300

    LoginButton & GithubLogo components

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import Button from 'react-toolbox/lib/button/Button';
+import GithubIcon from './GithubIcon';
+import './LoginButton.css';
+
+const authorizeUrl = 'https://github.com/login/oauth.authorize';
+const clientId = 'foobar';
+const scope = 'user';
 
 const LoginButton = ({}) => (
-  <div></div>
+  <Button
+    raised
+    accent
+    href={`${authorizeUrl}?client_id=${clientId}&scope=${scope}`}
+  >
+    <GithubIcon />
+    {' '}
+    Login with GitHub
+  </Button>
 )
 
 export default LoginButton;
