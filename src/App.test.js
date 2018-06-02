a8f5dbd3dda717b9cb7c5f945651fc2308da10c2
Rodrigo Cavichioli <rodrigoack@gmail.com>
2018-06-02 18:45:14 -0300

    enzyme isntall & test suite v1

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
 import App from './App';
+import Login from './Login';
+import Main from './Main';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  describe('without a token param', () => {
+    it('renders a Login view', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find(Login)).toHaveLength(1);
+    });
+  });
+
+  describe('with a token param', () => {
+    it('renders a Main view', () => {
+      const wrapper = shallow(<App />);
+      wrapper.setState({token: 'foobar'});
+      expect(wrapper.find(Main)).toHaveLength(1);
+    });
+  });
+})
