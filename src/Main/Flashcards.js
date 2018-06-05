7ff7dd1f993ba9f548bf348a252f699c68af7fad
Rodrigo Cavichioli <rodrigoack@gmail.com>
2018-06-04 23:29:29 -0300

    Flashcards component structure and style.

--- src/Main/Flashcards.js
+++ src/Main/Flashcards.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+// Components
+import Flashcard from './Flashcard';
+
+// Toolbox
+import Button from 'react-toolbox/lib/button/Button';
+import Chip from 'react-toolbox/lib/chip/Chip';
+
+// Styles
+import './Flashcards.css'
+
+
 const Flashcards = () => (
-  <div>Flashcards</div>
+  <div className="Flashcards">
+    <div className="Flashcards-content">
+      <div className="Flashcards-button">
+        <Button icon="keyboard_arrow_left" floating />
+      </div>
+      <Flashcard />
+      <div className="Flashcards-button">
+        <Button icon="keyboard_arrow_right" floating />
+      </div>
+      <div className="Flashcards-index">
+        <Chip>
+        </Chip>
+      </div>
+    </div>
+  </div>
 );
 
 export default Flashcards;
