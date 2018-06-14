2b402fe5fdaa51167b43d977e3285ace343cf3b1
Rodrigo Cavichioli <rodrigoack@gmail.com>
2018-06-13 22:59:38 -0300

    Flashcards tests

---
+++ src/Main/Flashcards/Flashcards.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
+
+import Flashcards from './Flashcards';
+import Flashcard from './Flashcard';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Flashcards />', () => {
+  it('renders a message when no tags', () => {
+    const wrapper = shallow(<Flashcards />);
+    expect(wrapper.text()).toEqual('Select a tag on the left!');
+  });
+
+  it('renders a message when no flashcards', () => {
+    const wrapper = shallow(<Flashcards tagId={42}/>);
+    expect(wrapper.text()).toEqual('Loading...');
+  });
+
+  it('renders a message when tag and empty flashcards', () => {
+    const wrapper = shallow(<Flashcards tagId={42} flashcards={[]} />);
+    expect(wrapper.text(Flashcard)).toEqual('add a flashcard for this tag');
+  });
+
+  it('renders a flashcard when tag and non-empty flashcards', () => {
+    const wrapper = shallow(
+      <Flashcards
+        tagId={42}
+        flashcards={[{
+          question: 'QQ',
+          answer: 'AA'
+        }]}
+      />
+    );
+    expect(wrapper.find(Flashcard).length).toEqual(1);
+  });
+});
